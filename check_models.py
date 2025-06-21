fddb388257ecb142825e9143ab7477c7ba7f5a3a
stepenkozv <stepenkozv@sibur.ru>
2025-06-22 01:03:35 +0300

    aa

--- check_models.py
+++ check_models.py
@@ -1,5 +1,5 @@
 import google.generativeai as genai
-from config import GEMINI_API_KEY # Убедись, что этот импорт работает
+from config import GEMINI_API_KEY
 
 genai.configure(api_key=GEMINI_API_KEY)
 
